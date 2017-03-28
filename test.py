47af8439b4ba421d6768ce55c743f7569f0db844
yanjingtui <meirunzhong@outlook.com>
2017-03-28 10:04:51 +0200

    watershed
    watershed

---
+++ test.py
@@ -0,0 +1,38 @@
+﻿import numpy as np
+import matplotlib.pyplot as plt
+from scipy import ndimage as ndi
+from skimage import morphology,feature
+
+#create two circle
+x, y = np.indices((80, 80))
+x1, y1, x2, y2 = 28, 28, 44, 52
+r1, r2 = 16, 20
+mask_circle1 = (x - x1)**2 + (y - y1)**2 < r1**2
+mask_circle2 = (x - x2)**2 + (y - y2)**2 < r2**2
+image = np.logical_or(mask_circle1, mask_circle2)
+
+#use the watershed
+distance = ndi.distance_transform_edt(image) #distance transform
+local_maxi =feature.peak_local_max(distance, indices=False, footprint=np.ones((3, 3)),
+                            labels=image)   #find the value max
+markers = ndi.label(local_maxi)[0] #init marker
+labels =morphology.watershed(-distance, markers, mask=image) #watershed based on distance
+
+fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(8, 8))
+axes = axes.ravel()
+ax0, ax1, ax2, ax3 = axes
+
+ax0.imshow(image, cmap=plt.cm.gray, interpolation='nearest')
+ax0.set_title("Original")
+ax1.imshow(-distance, cmap=plt.cm.jet, interpolation='nearest')
+ax1.set_title("Distance")
+ax2.imshow(markers, cmap=plt.cm.spectral, interpolation='nearest')
+ax2.set_title("Markers")
+ax3.imshow(labels, cmap=plt.cm.spectral, interpolation='nearest')
+ax3.set_title("Segmented")
+
+for ax in axes:
+    ax.axis('off')
+
+fig.tight_layout()
+plt.show()
